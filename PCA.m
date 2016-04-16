2f97af5e9f741bc06a0551c1445d0ce7b7de96b0
RoboticHuman <donomar@aucegypt.edu>
2016-04-16 18:07:24 +0200

    ROC curve and CCR against number of Eigens done

--- PCA.m
+++ PCA.m
@@ -1,64 +1,77 @@
 % data for MNIST
 trainingMat = loadMNISTImages('../MNIST/train-images-idx3-ubyte');
 trainingLabels = loadMNISTLabels('../MNIST/train-labels-idx1-ubyte');
-
 testingMat = loadMNISTImages('../MNIST/t10k-images-idx3-ubyte');
 testingLabels = loadMNISTLabels('../MNIST/t10k-labels-idx1-ubyte');
-
 trainingMat = transpose(trainingMat);
 testingMat = transpose(testingMat);
+
 %Beginig of PCA code
 
-meanOfTraining = mean(trainingMat);
+%trainingMat = trainingMat - (ones(size(trainingMat,1),1) * mean(trainingMat));
+%testingMat = testingMat - (ones(size(testingMat,1),1) * mean(trainingMat));
 
 covOfTraining = cov(trainingMat);
 
 [eigenVectorsMat, eigenValueVec] = eig(covOfTraining);
 
-%eigenVectorsMat = transpose(eigenVectorsMat);
-
-
-
 eigenValueVec = diag(eigenValueVec);
 
-%imshow( reshape(10*eigenVectorsMat(:,5),[28 28]) )
+for iterations = 784:-1:770
+    %fileID = fopen(strcat(int2str(iterations),'.txt'),'r');
+    fileName = strcat(int2str(iterations));
+    vec = eigenVectorsMat(:,iterations);
+    h = reshape(transpose(10*vec),[28 28]);
+    imwrite(h,strcat(fileName,'.png'));
+end
 
 subTrainingLabels = transpose(zeros(1,length(trainingLabels)));
 subTestingLabels = transpose(zeros(1,length(testingLabels)));
 prediction = zeros(length(testingMat),10);
-totalClass = transpose(zeros(1, 10));
 
-for numOfEigenVecs = 55:1:55
-   projectedTraining = trainingMat * eigenVectorsMat(:,length(eigenVectorsMat)-numOfEigenVecs:length(eigenVectorsMat));
-   %projectedTraining = projectedTraining - min(projectedTraining(:));
-   %projectedTraining = projectedTraining ./ max(projectedTraining(:));
-   projectedTesting = testingMat * eigenVectorsMat(:,length(eigenVectorsMat)-numOfEigenVecs:length(eigenVectorsMat));
-   %projectedTesting = projectedTesting - min(projectedTesting(:));
-   %projectedTesting = projectedTesting ./ max(projectedTesting(:));
-   for modelIndex = 0:1:9
-       % build Logistic model
-       subTrainingLabels(trainingLabels == modelIndex) = 1;
-       subTrainingLabels(trainingLabels ~= modelIndex) = 0;
-       subTestingLabels(testingLabels == modelIndex) = 1;
-       subTestingLabels(testingLabels ~= modelIndex) = 0;
-       %model = glmfit(projectedTraining, subTrainingLabels);
-       model = glmfit(projectedTraining, subTrainingLabels);
-       scores = glmval(model,projectedTesting,'logit');
-       [X, Y, T] = perfcurve(subTestingLabels,scores,1);
-       legendInfo{modelIndex+1} = num2str(modelIndex);
-       hold on 
-       graphROC =  plot(X,Y);
-       hold off
-       prediction(:,modelIndex+1) = glmval(model,projectedTesting,'logit');
-   end
+%legendForMNIST
+legendHandles = 1:1:10;
+for legendIndex = 1:1:10
+    legendForMNIST{legendIndex} = num2str(legendIndex-1);
 end
 
-legend(graphROC,legendInfo);
+numOfEigenVecs = [50 100 150 200 300 350];
 
-[M, I] = max(prediction, [], 2);
+CCRvsEigens = zeros(length(numOfEigenVecs),2);
 
-I = I-1
+for iterations = 1:1:length(numOfEigenVecs)
+   projectedTraining = trainingMat * eigenVectorsMat(:,length(eigenVectorsMat)-numOfEigenVecs(iterations):length(eigenVectorsMat));
+   %projectedTraining = projectedTraining - min(projectedTraining(:));
+   %projectedTraining = projectedTraining ./ max(projectedTraining(:));
+   projectedTesting = testingMat * eigenVectorsMat(:,length(eigenVectorsMat)-numOfEigenVecs(iterations):length(eigenVectorsMat));
+   for modelIndex = 0:1:9
+       % build Logistic model
+           subTrainingLabels(trainingLabels == modelIndex) = 1; subTrainingLabels(trainingLabels ~= modelIndex) = 0;
+           subTestingLabels(testingLabels == modelIndex) = 1; subTestingLabels(testingLabels ~= modelIndex) = 0;
+           scores = LogisticRegressor(projectedTraining,subTrainingLabels,projectedTesting); prediction(:,modelIndex+1) = scores;
+       %{ 
+       % ROC curves drawing
+       if(numOfEigenVecs(iterations) == 350)
+           [X, Y, T] = perfcurve(subTestingLabels,scores,1);
+           hold on 
+           legendHandles(modelIndex+1) = plot(X,Y);
+           hold off
+       end
+       %}
+   end
+   %CCR
+   [M, I] = max(prediction, [], 2);
+   I = I-1;
+   CCRvsEigens(iterations,2)=sum(I==testingLabels)/length(testingLabels);
+   CCRvsEigens(iterations,1)=numOfEigenVecs(iterations);
+end
 
-sum(I==testingLabels)/length(testingLabels) * 100
-
+%legend(legendHandles,legendForMNIST);
+%{
+%plotting for CCR against number of eigens
+plot(CCRvsEigens(:,1),CCRvsEigens(:,2));
+title('CCR vs Eigens');
+xlabel('Number Of Eigens');
+ylabel('CCR');
+%}
 disp('ended');
