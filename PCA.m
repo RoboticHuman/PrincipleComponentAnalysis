bc8fbfab68eb78919ec80eae960a8800cb8dfa1f
RoboticHuman <donomar@aucegypt.edu>
2016-04-16 11:23:42 +0200

    ROC curves done, still testing validity of the calssifier

--- PCA.m
+++ PCA.m
@@ -1,9 +1,9 @@
 % data for MNIST
-trainingMat = loadMNISTImages('/home/robotichuman/PatternAnalysis/MNIST/train-images-idx3-ubyte');
-trainingLabels = loadMNISTLabels('/home/robotichuman/PatternAnalysis/MNIST/train-labels-idx1-ubyte');
+trainingMat = loadMNISTImages('../MNIST/train-images-idx3-ubyte');
+trainingLabels = loadMNISTLabels('../MNIST/train-labels-idx1-ubyte');
 
-testingMat = loadMNISTImages('/home/robotichuman/PatternAnalysis/MNIST/t10k-images-idx3-ubyte');
-testingLabels = loadMNISTLabels('/home/robotichuman/PatternAnalysis/MNIST/t10k-labels-idx1-ubyte');
+testingMat = loadMNISTImages('../MNIST/t10k-images-idx3-ubyte');
+testingLabels = loadMNISTLabels('../MNIST/t10k-labels-idx1-ubyte');
 
 trainingMat = transpose(trainingMat);
 testingMat = transpose(testingMat);
@@ -22,21 +22,30 @@ eigenValueVec = diag(eigenValueVec);
 %imshow( reshape(10*eigenVectorsMat(:,5),[28 28]) )
 
 subTrainingLabels = transpose(zeros(1,length(trainingLabels)));
-
+subTestingLabels = transpose(zeros(1,length(testingLabels)));
 prediction = zeros(length(testingMat),10);
+totalClass = transpose(zeros(1, 10));
 
-for numOfEigenVecs = 99:1:100
+for numOfEigenVecs = 55:1:55
    projectedTraining = trainingMat * eigenVectorsMat(:,length(eigenVectorsMat)-numOfEigenVecs:length(eigenVectorsMat));
-   projectedTraining = projectedTraining - min(projectedTraining(:));
-   projectedTraining = projectedTraining ./ max(projectedTraining(:));
+   %projectedTraining = projectedTraining - min(projectedTraining(:));
+   %projectedTraining = projectedTraining ./ max(projectedTraining(:));
    projectedTesting = testingMat * eigenVectorsMat(:,length(eigenVectorsMat)-numOfEigenVecs:length(eigenVectorsMat));
-   projectedTesting = projectedTesting - min(projectedTesting(:));
-   projectedTesting = projectedTesting ./ max(projectedTesting(:));
+   %projectedTesting = projectedTesting - min(projectedTesting(:));
+   %projectedTesting = projectedTesting ./ max(projectedTesting(:));
    for modelIndex = 0:1:9
        % build Logistic model
        subTrainingLabels(trainingLabels == modelIndex) = 1;
        subTrainingLabels(trainingLabels ~= modelIndex) = 0;
+       %subTestingLabels(testingLabels == modelIndex) = 1;
+       %subTestingLabels(testingLabels ~= modelIndex) = 0;
+       %model = glmfit(projectedTraining, subTrainingLabels);
        model = glmfit(projectedTraining, subTrainingLabels);
+       scores = glmval(model,projectedTesting,'logit');
+       [X, Y, T] = perfcurve(testingLabels,scores,1);
+       hold on 
+       plot(X,Y);
+       hold off
        prediction(:,modelIndex+1) = glmval(model,projectedTesting,'logit');
    end
 end
